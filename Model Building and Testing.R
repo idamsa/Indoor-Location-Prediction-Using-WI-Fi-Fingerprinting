caaa8bd956716c73dc81a994c2b56bc36b3d2634
Ignacio <ignacio@ubiqum.com>
2020-02-21 14:32:56 +0100

    All scripts reviewed. Starting adding suggestions

--- Model Building and Testing.R
+++ Model Building and Testing.R
@@ -6,6 +6,8 @@ set.seed(123)
 # 1. KNN BUILDING ---- 
 # We will remove the floor, lat and long from the training as they will be unknown for the actual data
 # Check results on test dataset acc: 99.90165 
+#### Ignacio: Ioana, why you have chosen k=1? Do you have any prior benchmark 
+#### which shows you that k=1 is the best option?
 
 system.time(knnFitTest <- knn(train = dfTrainingStand[ , -which(names(dfTrainingStand) %in% c("LONGITUDE","LATITUDE","FLOOR"))], 
                               test = dfTestStand[ , -which(names(dfTestStand) %in% c("LONGITUDE","LATITUDE","FLOOR"))],
@@ -23,20 +25,35 @@ knnFitValid <- knn(train=dfTrainingStand[, -which(names(dfTrainingStand) %in% c(
                    cl=dfTrainingStand$BUILDINGID, k=1)
 print(knnCMV <- confusionMatrix(knnFitValid, dfValidationStand$BUILDINGID)) # Confusion Matrix
 
+#### Ignacio: Ioana, here it becomes clear that your initial goal was to benchmark
+#### the computational time needed to train a knn model. Here you test different
+#### values in order to determine the best option.
 # Optimize KNN --- ACC AND KAPPA GO DOWN AFTER K=1 BOTH IN TEST AND VALID
 # Decided to keep the nr of neighbours 1 # used this function for the other knn models too
 
-i=1
-k.optm=1
+i<-1
+k.optm<-1
 for (i in 1:9){
   knn.mod <- knn(train = dfTrainingStand[, -which(names(dfTrainingStand) %in% c("LONGITUDE","LATITUDE","FLOOR"))], 
                  test = dfValidationStand[, -which(names(dfValidationStand) %in% c("LONGITUDE","LATITUDE","FLOOR"))],
                  cl = dfTrainingStand$BUILDINGID, k=i)
   k.optm[i] <- 100 * sum(dfValidationStand$BUILDINGID ==  knn.mod)/NROW(dfValidationStand$BUILDINGID)
   k=i
-  cat(k,"=",k.optm[i],"
-      ")
+  cat(k,"=",k.optm[i],"")
 }
+
+#### Ignacio: Ioana, added a plot to show the performance of the k-nn model as
+#### a function of the k value.
+
+k_values <- seq(1:9)
+acc <- k.optm
+res <-data.frame(k=k_values,accuracy=acc)
+
+ggplot(res,aes(x=k,y=accuracy)) + geom_line(size=1,color="blue") +
+  geom_point(size=1.2,color="blue") +
+  scale_x_continuous(breaks = c(1:9)) +
+  ggtitle("Performance of the k-nn model on the training set")
+
 
 # 2. SVM BUILDING (CHOSEN) ----
 set.seed(123)
@@ -44,13 +61,18 @@ ctrl <- trainControl(method = "cv", number = 10)
 system.time(svmFit <- train(BUILDINGID ~.,
                             data = dfTrainingStand[, -which(names(dfTrainingStand) %in% c("LONGITUDE","LATITUDE","FLOOR"))],
                             method = "svmLinear",
-                            trControl = ctrl
+                            trControl = ctrl,
+                            verbose = TRUE
 
 
 )
 )
 
-svmFit <- readRDS("svmBuildingFinal.rds")
+#### Ignacio: Check if the model exists, otherwise, load it from the folder.
+if (!exists("svmFit")) {
+  svmFit <- readRDS("svmBuildingFinal.rds")
+}
+
 # Check results on validation dataset # 99 % acc  kappa 0.9897  
 svmTest <- predict(svmFit ,newdata = dfTestStand[, -which(names(dfTestStand) %in% c("LONGITUDE","LATITUDE","FLOOR"))])
 print(svmCMT <- confusionMatrix(svmTest, dfTestStand$BUILDINGID)) # Confusion Matrix
@@ -99,7 +121,12 @@ system.time(svmFitFloor <- train(FLOOR ~ .,
 # user  system elapsed
 # 33.89    2.20   37.53
 
-svmFitFloor <- readRDS("svmFloorFinal.rds")
+#### Ignacio: Same as before
+if (!exists("svmFitFloor")) {
+  svmFit <- readRDS("svmFloorFinal.rds")
+}
+
+#svmFitFloor <- readRDS("svmFloorFinal.rds")
 
 # Check results on test dataset  Accuracy : 0.9932   ,   Kappa : 0.9926 
 svmTestFloor <- predict(svmFitFloor, newdata = dfTestStand[, -which(names(dfTestStand) %in% c("LONGITUDE","LATITUDE"))])
@@ -108,7 +135,7 @@ print(svmCMTFloor <- confusionMatrix(svmTestFloor , dfTestStand$FLOOR)) # Confus
 # Check results on validation dataset Accuracy : 0.9932   ,Kappa : 0.9926   
 svmValidFloor <- predict(svmFitFloor ,newdata = dfValidationStand[, -which(names(dfValidationStand) %in% c("LONGITUDE","LATITUDE"))])
 print(svmCMVFloor <- confusionMatrix(svmValidFloor, dfValidationStand$FLOOR)) # Confusion Matrix
-plotconfusion(svmCMVFloor)
+#plotconfusion(svmCMVFloor)
 # Save Model
 saveRDS(svmFitFloor, file="svmFloor.rds")
 
@@ -132,11 +159,15 @@ bestmtry_rf_lat <- tuneRF(dfTraining[, -which(names(dfTraining) %in% c("LATITUDE
                       trace=TRUE,
                       plot=T)
 
+#### Ignacio: How to do this more generic in the next step.
+bestmtry_rf_lat <- as.data.frame(bestmtry_rf_lat)
+
 system.time(rfFitLatitude <- randomForest(x = dfTraining[, -which(names(dfTraining) %in% c("LONGITUDE","LATITUDE"))],
                                           y = dfTraining$LATITUDE,
                                           ntrees = 100,
                                           importance = T,
-                                          mtry = 88
+                                          # mtry = 88
+                                          mtry = bestmtry_rf_lat[which.min(bestmtry_rf_lat$OOBError),]$mtry
 )
 )
 # user  system elapsed
@@ -144,6 +175,8 @@ system.time(rfFitLatitude <- randomForest(x = dfTraining[, -which(names(dfTraini
 
 # Bring model 
 
+#### Ignacio: Again, check if the variable exists, then if not, load the search
+#### for the model and load it.
 rfFitLatitude <- readRDS("rfLatitudeFinal.rds")
 
 # Predict and evaluate on Test
@@ -200,16 +233,15 @@ set.seed(123)
 bestmtry_rf_long <- tuneRF(dfTraining,
                       dfTraining$LONGITUDE, ntreeTry=100,stepFactor=2,improve=0.05,trace=TRUE, plot=T)
 
-# mtry OOBError
-# 88    88 5.536374
-# 175  175 5.743512
-# 350  350 7.065408
+bestmtry_rf_long <- as.data.frame(bestmtry_rf_long)
+
 
 system.time(rfFitLongitude <- randomForest(x = dfTraining[, -which(names(dfTraining) %in% c("LONGITUDE"))],
                                            y = dfTraining$LONGITUDE,
                                            ntrees = 100,
                                            importance = T,
-                                           mtry = 88
+                                           #mtry = 88
+                                           mtry = bestmtry_rf_long[which.min(bestmtry_rf_long$OOBError),]$mtry
 )
 )
 
@@ -218,7 +250,11 @@ system.time(rfFitLongitude <- randomForest(x = dfTraining[, -which(names(dfTrain
 
 # Bring Model
 
-rfFitLongitude <- readRDS("rfLongitudeFinal.rds")
+#### Ignacio: Same as before.
+if (!exists("rfFitLongitude")) {
+  svmFit <- readRDS("rfLongitudeFinal.rds")
+}
+#rfFitLongitude <- readRDS("rfLongitudeFinal.rds")
 
 # Predict and evaluate on Test
 rfTestLongitude <- predict(rfFitLongitude, dfTest)
@@ -260,3 +296,57 @@ errors_longitude_knnValid <- as.data.frame(dfValidationStand$LONGITUDE - knnVali
 
 # Save model
 saveRDS(knnFitLongitude, file = "knnLongitude.rds")
+
+#### TO DO:
+####
+#### Plot the where the observations were taken in both datasets. You made this plot
+#### Why you didn't include it, even on a separate script?
+####
+#### Missing the confussion matrix plotting. Where the errors took place?
+#### Use an scatter3D plot and use colors to codify if the observation was correct
+#### or not. Are the models making mistakes in the same place? 
+####
+
+dfValidation$knnFitValid <- ( knnFitValid == dfValidationStand$BUILDINGID ) 
+dfValidation$svmValid    <- ( svmValid    == dfValidationStand$BUILDINGID )
+
+plot_ly(dfValidation, x = ~LONGITUDE, y = ~LATITUDE, z = ~FLOOR, color = ~knnFitValid, colors = c('green', 'red')) %>%
+  add_markers() %>%
+  layout(title = 'Predicted BUILDINGID from K-NN model',
+    scene = list(xaxis = list(title = 'Longitude'),
+                      yaxis = list(title = 'Latitude'),
+                      zaxis = list(title = 'Floor')))
+
+plot_ly(dfValidation, x = ~LONGITUDE, y = ~LATITUDE, z = ~FLOOR, color = ~svmValid, colors = c('green', 'red')) %>%
+  add_markers() %>%
+  layout(title = 'Predicted BUILDINGID from SVM model',
+         scene = list(xaxis = list(title = 'Longitude'),
+                      yaxis = list(title = 'Latitude'),
+                      zaxis = list(title = 'Floor')))
+
+dfValidation$svmValidFloor    <- ( svmValidFloor == dfValidation$FLOOR )
+dfValidation$knnFitFloorValid <- ( knnFitFloorValid == dfValidation$FLOOR )
+
+plot_ly(dfValidation, x = ~LONGITUDE, y = ~LATITUDE, z = ~FLOOR, color = ~knnFitFloorValid, colors = c('green', 'red')) %>%
+  add_markers() %>%
+  layout(title = 'Predicted FLOOR from K-NN model',
+         scene = list(xaxis = list(title = 'Longitude'),
+                      yaxis = list(title = 'Latitude'),
+                      zaxis = list(title = 'Floor')))
+
+plot_ly(dfValidation, x = ~LONGITUDE, y = ~LATITUDE, z = ~FLOOR, color = ~svmValidFloor, colors = c('green', 'red')) %>%
+  add_markers() %>%
+  layout(title = 'Predicted FLOOR from SVM model',
+         scene = list(xaxis = list(title = 'Longitude'),
+                      yaxis = list(title = 'Latitude'),
+                      zaxis = list(title = 'Floor')))
+
+#### The same as well for the location error. Compute the location error by the
+#### Euclidian distance for each observation, and then make an scatter plot 
+#### of each predicted observation with a color proportional to the error. In
+#### addition, to prevent a continuoos scale and making difficult to discriminate
+#### over error sizes, you can bin the total error into "reasonable" sizes and
+#### color the observations according to those error sizes.
+####
+#### Also include a comparison plot of the distribution of errors by each model
+#### and identify outliyers. It will be great to do it with plotly.
